36a6244c163bb818c47aca3d21fef6684b65b6e1
Didii Cosmin <cosmin.didii@gmail.com>
2013-03-16 13:58:23 +0200

    added temporary console

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<title> Discovery console </title>
+<body style = "background-color: azure">
+
+<textarea style= "position: absolute; height: 90%; width: 99%; opacity:0.6" 
+	id = "console">
+
+</textarea>
+
+<input style= "position: absolute; top:93%; height: 5%; width: 99%; opacity:0.6" 
+	id = "consoleInput">
+
+</input>
+
+</body>
+</html>
+
+
